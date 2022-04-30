cac21e4f4daa1a675f5408e6b321e853fff7c8be
JonathanSh <jonatanshimon@gmail.com>
2022-04-30 22:26:43 +0300

    typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (ImportError, OSError) as e:
 
 setup(
     name='elf_to_shellcode',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['pyelftools'],
