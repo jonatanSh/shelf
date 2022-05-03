d90c5ca8a4ce04705378b49c60e65a4d3321a92d
jonatansh <jonatanshimon@gmail.com>
2022-05-03 22:22:04 +0300

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except (ImportError, OSError) as e:
 
 setup(
     name='elf_to_shellcode',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['pyelftools'],
