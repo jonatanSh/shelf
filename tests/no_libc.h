624bae376383c38103792fa745ec8f1fb7be0405
jonatansh <jonatanshimon@gmail.com>
2022-05-14 00:38:49 +0300

    start adding files for no relocations

---
+++ tests/no_libc.h
@@ -0,0 +1,12 @@
+#ifndef NO_LIBC
+#define NO_LIBC
+#include <sys/syscall.h>
+#if defined(i386) || defined(__i386__) || defined(__i386) || defined(_M_IX86)
+    #include "./no_libc/i386.h"
+#endif
+
+#define print_out(message, size) {\
+    my_syscall5(SYS_write, 1, message, size, 0, 0);\
+}\
+
+#endif
---
+++ tests/no_relocations.c
@@ -0,0 +1,5 @@
+#include "./no_libc.h"
+
+void main() {
+    print_out("Hello\n", 6);
+}
