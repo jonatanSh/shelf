1904d17c9f648f2bd536978671ee40427c27af66
jonatansh <jonatanshimon@gmail.com>
2022-05-14 18:43:02 +0300

    support no relocations

--- tests/run_elf_feature_tests.sh
+++ tests/run_elf_feature_tests.sh
@@ -4,3 +4,5 @@ echo Running intel x32 shellcode
 qemu-i386-static ../outputs/shellcode_loader_intel_x32.out ../outputs/elf_features_intel_x32.out.shellcode
 echo Running intel x64 shellcode
 qemu-x86_64-static ../outputs/shellcode_loader_intel_x64.out ../outputs/elf_features_intel_x64.out.shellcode
+echo Running no relocation tests
+qemu-i386-static ../outputs/shellcode_loader_intel_x32.out ../outputs/no_relocations_intel_x32.out.shellcode 
