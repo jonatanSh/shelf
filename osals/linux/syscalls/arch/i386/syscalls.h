652cd4db0db81277d5263b77ad0c7b3b6c8c80f4
jonatansh <jonatanshimon@gmail.com>
2022-08-05 18:16:57 +0300

    test are working

--- osals/linux/syscalls/arch/i386/syscalls.h
+++ osals/linux/syscalls/arch/i386/syscalls.h
@@ -23,7 +23,7 @@
 #define my_syscall5(num, arg1, arg2, arg3, arg4, arg5)                        \
 ({                                                                            \
 	long _ret;                                                            \
-	register long _num asm("eax") = (num);                                \
+	register long _num asm("eax") = (long)(num);                                \
 	register long _arg1 asm("ebx") = (long)(arg1);                        \
 	register long _arg2 asm("ecx") = (long)(arg2);                        \
 	register long _arg3 asm("edx") = (long)(arg3);                        \
@@ -31,7 +31,7 @@
 	register long _arg5 asm("edi") = (long)(arg5);                        \
 									      \
 	asm volatile (                                                        \
-		"int 0x80\n"                                                 \
+		"int $0x80\n"                                                 \
 		: "=a" (_ret)                                                 \
 		: "r"(_arg1), "r"(_arg2), "r"(_arg3), "r"(_arg4), "r"(_arg5), \
 		  "0"(_num)                                                   \
