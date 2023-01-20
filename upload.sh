b99b279e4c734557a8aac17f416c786d8a774db6
jonatansh <jonatanshimon@gmail.com>
2023-01-20 23:35:41 +0200

    switch to python2

--- upload.sh
+++ upload.sh
@@ -4,4 +4,4 @@
 ./build.sh
 
 # uploading
-python3 -m twine upload $(find dist/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+python -m twine upload $(find dist/ -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ") --verbose
