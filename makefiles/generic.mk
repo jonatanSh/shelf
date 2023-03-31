0a5fd5123f2c6c5d9253a6dc428db886e751732f
jonatansh <jonatanshimon@gmail.com>
2023-03-31 14:20:49 +0300

    start refactoring makefile

--- makefiles/generic.mk
+++ makefiles/generic.mk
@@ -17,30 +17,34 @@ shellcode_%: dir_guard
 	python3 -m shelf --input $(OUTPUT_DIRECTORY)/$(subst shellcode_,,$@).out.eshelf --output $(OUTPUT_DIRECTORY)/$(subst shellcode_,,$@).out.eshelf.shellcode --output-format eshelf
 	python3 -m shelf --input $(OUTPUT_DIRECTORY)/$(subst shellcode_,,$@).out --output $(OUTPUT_DIRECTORY)/$(subst shellcode_,,$@).out.rwx_bypass.shellcode --mitigation-bypass rwx
 
+geneirc_compile:
+	$(COMPILER) $(COMPILER_FLAGS) $(FILES) -o $(OUTPUT_FILE)
+
+geneirc_compile_eshelf:
+	$(COMPILER) $(COMPILER_FLAGS) $(FILES) -o $(OUTPUT_FILE).eshelf -DESHELF
+
+compile_all:
+	$(MAKE) geneirc_compile COMPILER="$(COMPILER)" COMPILER_FLAGS="$(COMPILER_FLAGS) -static" FILES="$(FILES)" OUTPUT_FILE="$(OUTPUT_FILE)"
+	$(MAKE) geneirc_compile_eshelf COMPILER="$(COMPILER)" COMPILER_FLAGS="$(COMPILER_FLAGS) -static" FILES="$(FILES)" OUTPUT_FILE="$(OUTPUT_FILE)"
 
 mips_%: mini_loader_mips mini_loader_mipsbe
-	$(MIPS_CC) $(CFLAGS) $(C_FILES) -static -BE $(subst mips_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out
-	$(MIPS_CC) $(CFLAGS) $(C_FILES) -static -BE $(subst mips_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out.eshelf -DESHELF
+	$(MAKE) compile_all COMPILER="$(MIPS_CC)" COMPILER_FLAGS="$(CFLAGS) -BE" FILES="$(subst mips_,,$@).c $(C_FILES)" OUTPUT_FILE="$(OUTPUT_DIRECTORY)$@.out"
 	$(MAKE) shellcode_$@
 
 
 intel_x32_%: mini_loader_x32
-	$(X32_CC) -masm=intel $(CFLAGS) $(subst intel_x32_,,$@).c $(C_FILES) -static -o $(OUTPUT_DIRECTORY)$@.out
-	$(X32_CC) -masm=intel $(CFLAGS) $(subst intel_x32_,,$@).c $(C_FILES) -static -o $(OUTPUT_DIRECTORY)$@.out.eshelf -DESHELF
+	$(MAKE) compile_all COMPILER="$(X32_CC)" COMPILER_FLAGS="$(CFLAGS) -masm=intel" FILES="$(C_FILES) $(subst intel_x32_,,$@).c" OUTPUT_FILE="$(OUTPUT_DIRECTORY)$@.out"
 	$(MAKE) shellcode_$@
 
 
 intel_x64_%: mini_loader_x64
-	$(X64_CC) -masm=intel $(CFLAGS) $(C_FILES) -static $(subst intel_x64_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out
-	$(X64_CC) -masm=intel $(CFLAGS) $(C_FILES) -static $(subst intel_x64_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out.eshelf -DESHELF
+	$(MAKE) compile_all COMPILER="$(X64_CC)" COMPILER_FLAGS="$(CFLAGS) -masm=intel" FILES="$(C_FILES) $(subst intel_x64_,,$@).c" OUTPUT_FILE="$(OUTPUT_DIRECTORY)$@.out"
 	$(MAKE) shellcode_$@
 
 arm32_%: mini_loader_arm_x32
-	$(ARM_CC) $(CFLAGS) $(C_FILES) -static $(subst arm32_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out
-	$(ARM_CC) $(CFLAGS) $(C_FILES) -static $(subst arm32_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out.eshelf -DESHELF
+	$(MAKE) compile_all COMPILER="$(ARM_CC)" COMPILER_FLAGS="$(CFLAGS)" FILES="$(C_FILES) $(subst arm32_,,$@).c" OUTPUT_FILE="$(OUTPUT_DIRECTORY)$@.out"
 	$(MAKE) shellcode_$@
 
 aarch64_%: mini_loader_arm_x64
-	$(AARCH64_CC) $(CFLAGS) $(C_FILES) -static $(subst aarch64_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out
-	$(AARCH64_CC) $(CFLAGS) $(C_FILES) -DESHELF -static $(subst aarch64_,,$@).c -o $(OUTPUT_DIRECTORY)$@.out.eshelf
+	$(MAKE) compile_all COMPILER="$(AARCH64_CC)" COMPILER_FLAGS="$(CFLAGS)" FILES="$(C_FILES) $(subst aarch64_,,$@).c" OUTPUT_FILE="$(OUTPUT_DIRECTORY)$@.out"
 	$(MAKE) shellcode_$@
