89651b70a91716de8328e6d0d72bd744adbd9099
jonatansh <jonatanshimon@gmail.com>
2023-03-31 16:46:44 +0300

    change run example script

--- examples/run_example.sh
+++ examples/run_example.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Run the example for target arch mips
-qemu-mips-static ../outputs/shellcode_loader_mips.out ../outputs/example_mipsbe.out.shellcode
+qemu-mips-static ../outputs/shellcode_loader_mips.out ../outputs/mips_example.out.shellcode
