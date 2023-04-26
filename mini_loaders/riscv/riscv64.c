8162bcbb906ae33508dd7497a41bb0c5f354a89c
jonatansh <jonatanshimon@gmail.com>
2023-04-26 23:21:26 +0300

    add arch return

--- mini_loaders/riscv/riscv64.c
+++ mini_loaders/riscv/riscv64.c
@@ -3,6 +3,7 @@
 
 void startup_code(size_t main_ptr, int argc, void * argv) {
     size_t return_address;
+    size_t _out;
     ARCH_FUNCTION_ENTER(&return_address);
     TRACE("Inside startup code going to call %x", main_ptr);
     register size_t a4 asm("a4") = (size_t)(main_ptr);
@@ -13,10 +14,11 @@ void startup_code(size_t main_ptr, int argc, void * argv) {
 #if !defined(GLIBC_STARTUP)
     asm volatile(
         "jalr a4\n"
-        : :
+        : "=r"(_out) :
         "r"(a4), "r"(a0), "r"(a1), "r"(a2)
         : "ra"
     );
 #endif
+    ARCH_RETURN(_out);
     ARCH_FUNCTION_EXIT(return_address);
 }
