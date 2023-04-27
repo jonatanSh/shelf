b435d8729175a97baffdb910cc955d8c6794d5bb
jonatansh <jonatanshimon@gmail.com>
2023-04-28 00:58:07 +0300

    mips partial fix

--- mini_loaders/mips/mips.c
+++ mini_loaders/mips/mips.c
@@ -1,8 +1,9 @@
 #include "../generic_loader.h"
 #include "mips.h"
 
-void loader_call_main(size_t main_ptr, int argc, void * argv) {
+size_t loader_call_main(size_t main_ptr, int argc, void * argv) {
     ARCH_FUNCTION_ENTER();
+    size_t _out;
     TRACE("Inside startup code going to call %x", main_ptr);
     register size_t t9 asm("t9") = (size_t)(main_ptr);
     register size_t a0 asm("a0") = (size_t)(main_ptr);
@@ -12,9 +13,10 @@ void loader_call_main(size_t main_ptr, int argc, void * argv) {
 #if !defined(GLIBC_STARTUP)
     asm volatile(
         "jalr $t9\n"
-        : :
+        : "=r"(_out) :
         "r"(t9), "r"(a0), "r"(a1), "r"(a2)
         : "ra"
     );
 #endif
+    return _out;
 }
