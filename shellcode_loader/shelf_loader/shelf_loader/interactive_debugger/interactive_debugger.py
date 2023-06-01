4ede64ce35638574bd71cc5cae35e9e3295a5a43
jonatansh <jonatanshimon@gmail.com>
2023-06-01 23:00:58 +0300

    start adding more commands to the interactive shell

--- shellcode_loader/shelf_loader/shelf_loader/interactive_debugger/interactive_debugger.py
+++ shellcode_loader/shelf_loader/shelf_loader/interactive_debugger/interactive_debugger.py
@@ -18,9 +18,11 @@ class InteractiveDebugger(object):
         self.loader = loader
         self.commands = {
             '?': self.help,
-            'help': self.help
+            'help': self.help,
+            'rebase': self.rebase
         }
         self.auto_complete = get_auto_complete(self.commands.keys())
+        self.base_address = 0x0
         readline.parse_and_bind("history")
         readline.set_completer(self.auto_complete)
 
@@ -37,8 +39,12 @@ class InteractiveDebugger(object):
             except KeyboardInterrupt:
                 should_break = True
 
+    @staticmethod
+    def shell_read_arguments(prompt):
+        return input("{} ".format(prompt))
+
     def handle_commands(self):
-        input_line = input("> ")
+        input_line = self.shell_read_arguments(">")
         command = self.commands.get(input_line)
         if not command:
             self.shell_print("Unknown command: {} use ? for help".format(input_line))
@@ -50,3 +56,8 @@ class InteractiveDebugger(object):
         self.shell_print("Commands:")
         for command in self.commands:
             self.shell_print(command)
+
+    def rebase(self):
+        value = int(self.shell_read_arguments('> Enter the loading address in hex'), 16)
+        self.shell_print("Rebasing to: {}".format(hex(value)))
+        self.base_address = value
