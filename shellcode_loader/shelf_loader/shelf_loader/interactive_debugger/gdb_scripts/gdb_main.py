fe81addd2f35dc2166e11ed486f5ed001526246f
jonatansh <jonatanshimon@gmail.com>
2023-06-11 23:50:32 +0300

    fixes

--- shellcode_loader/shelf_loader/shelf_loader/interactive_debugger/gdb_scripts/gdb_main.py
+++ shellcode_loader/shelf_loader/shelf_loader/interactive_debugger/gdb_scripts/gdb_main.py
@@ -1,1 +1,15 @@
-print("YYOYOYOY")
+import gdb
+
+HEADER = "SHELF LOADER GDB INTEGRATION"
+print(HEADER)
+gdb_ms = """
+x/20i $pc
+si
+"""
+
+
+def gdb_define(command_name, gdb_command):
+    gdb.execute("define {}\n{}\nend".format(command_name, "\n".format(gdb_command)))
+
+
+gdb_define('ms', gdb_ms)
