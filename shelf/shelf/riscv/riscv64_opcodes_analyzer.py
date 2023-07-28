6f2f2e82435c5c9fd3d24329de9c0384d09f69ad
jonatansh <jonatanshimon@gmail.com>
2023-07-28 22:02:47 +0300

    getting there

--- shelf/shelf/riscv/riscv64_opcodes_analyzer.py
+++ shelf/shelf/riscv/riscv64_opcodes_analyzer.py
@@ -53,13 +53,37 @@ class Riscv64OpcodesAnalyzer(OpcodeAnalyzer):
             f_offset_relative = f_offset - self.shelf.opcodes_start_address
             # Now going to construct a relative pc load
             pc_relative = v_offset - f_offset
+            if pc_relative < 0:
+                # TODO figure out if this is ok
+                self.logger.warning("Riscv invalid pc relative offset")
+                continue
             lui_instruction = self.shelf.address_utils.unpack(
                 "I",
-                shellcode_data[f_offset_relative:f_offset_relative+4]
-            )
+                shellcode_data[f_offset_relative:f_offset_relative + 4]
+            )[0]
             # Lui and auipc differs only in the 6th bit
             # This mask will convert to instruction to auipc
-            auipc_instruction = lui_instruction & 0xffffffdf
+            auipc_instruction = lui_instruction & 0x00000fdf
+            assert pc_relative < (1 << 20)
+            auipc_instruction += (pc_relative << 12)
+            auipc_instruction_packed = self.shelf.address_utils.pack("I", auipc_instruction)
+            self.logger.info("Patching lui={} with auipc={}".format(
+                hex(lui_instruction),
+                hex(auipc_instruction)
+            ))
+            # Replacing the instruction with a relative auipc instruction
+            shellcode_data = shellcode_data[:f_offset_relative] + auipc_instruction_packed + shellcode_data[
+                                                                                             f_offset_relative + 4:]
+            # Now going to override the addiu instruction
+            ld_instruction = self.shelf.address_utils.unpack(
+                "I",
+                shellcode_data[f_offset_relative + 4:f_offset_relative + 8]
+            )[0]
+            patched_ld_instruction = (ld_instruction & 0xfffff) << 12
+            patched_ld_instruction_packed = self.shelf.address_utils.pack("I", patched_ld_instruction)
+
+            shellcode_data = shellcode_data[:f_offset_relative + 4] + patched_ld_instruction_packed + shellcode_data[
+                                                                                                      f_offset_relative + 8:]
 
         return shellcode_data
 
